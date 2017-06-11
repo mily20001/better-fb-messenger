1fbd5d871e745d9f9db9c9a155b19aad1841f110
mily20001 <mily20001@gmail.com>
2017-06-12 00:51:41 +0200

    fix(run-server): now script is more realiable

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
-node_modules/.bin/nodemon --watch backend/src/ --exec "node_modules/.bin/babel -o ./backend/build/server.js ./backend/src/server.js" &
+./node_modules/.bin/mkdirp ./backend/build/ ./frontend/build/
+
+build1_cmd="node_modules/.bin/babel -o ./backend/build/server.js ./backend/src/server.js";
+
+eval "${build1_cmd}";
+
+node_modules/.bin/nodemon --watch backend/src/ --exec "${build1_cmd}" &
 node_modules/.bin/nodemon --watch backend/build/ ./backend/build/server.js
