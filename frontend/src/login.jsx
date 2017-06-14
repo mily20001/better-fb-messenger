219ba4fa3b9accb63f6988ab7a6b133edf4b94ba
mily20001 <mily20001@gmail.com>
2017-06-15 01:25:06 +0200

    feat(login): add login page component

--- frontend/src/login.jsx
+++ frontend/src/login.jsx
@@ -1,4 +1,15 @@
 import React from 'react';
+import {
+    Button,
+    Grid,
+    Row,
+    Col,
+    Form,
+    FormControl,
+    FormGroup,
+    ControlLabel,
+    Well,
+} from 'react-bootstrap';
 
 export default class Login extends React.Component {
     constructor(props) {
@@ -8,13 +19,55 @@ export default class Login extends React.Component {
             password: '',
         };
     }
+
+    handleInputChange(field, newValue) {
+        this.setState({ [field]: newValue });
+    }
+
     render() {
+        const wellStyle = { borderRadius: '20px' };
+        const gridStyle = { position: 'relative', top: '50%', transform: 'translateY(-50%)' };
         return (
-            <div>
-                <div>Login here:</div>
-                <b>Email: </b> <input type="text" /> <br />
-                <i>Password:</i> <input type="password" />
-            </div>
+            <Grid style={gridStyle}>
+                <Row>
+                    <Col xs={12} sm={10} md={8} smOffset={1} mdOffset={2}>
+                        <Well style={wellStyle}>
+                            <h2>Facebook login</h2>
+                            <Form horizontal>
+                                <FormGroup>
+                                    <Col componentClass={ControlLabel} xs={2}>
+                                        Email:
+                                    </Col>
+                                    <Col sm={10} xs={12}>
+                                        <FormControl
+                                            type="email"
+                                            onChange={e => this.handleInputChange('mail', e)}
+                                            placeholder="Email"
+                                        />
+                                    </Col>
+                                </FormGroup>
+                                <FormGroup>
+                                    <Col componentClass={ControlLabel} xs={2}>
+                                        Password:
+                                    </Col>
+                                    <Col sm={10} xs={12}>
+                                        <FormControl
+                                            type="password"
+                                            onChange={e => this.handleInputChange('password', e)}
+                                            placeholder="Password"
+                                        />
+                                    </Col>
+                                </FormGroup>
+                                <FormGroup>
+                                    <Col xs={12} sm={3} smOffset={9}>
+                                        <Button bsSize="large" block>Login</Button>
+                                    </Col>
+                                </FormGroup>
+                            </Form>
+                        </Well>
+                    </Col>
+                </Row>
+            </Grid>
         );
     }
 }
